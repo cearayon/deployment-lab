08ad50ac1da755e2cbbdc0ef9cd40ebd819781a8
Carlo Arayon <cearayon@gmail.com>
2022-07-20 13:41:45 -0700

    added styling

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../front-end/index.html'))
 })
 
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../front-end/styles.css'))
+})
+
 const port = process.env.PORT || 5501
 
 app.listen(port, () => {
